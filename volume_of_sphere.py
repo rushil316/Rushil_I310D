e12231657f29b86ab4c91f6294239305f4ccf72e
Rushil Vankamamidi <rushilvankamamidi@Rushils-MacBook-Air.local>
2025-04-09 13:44:19 -0500

    calculated the volume of the sphere

--- volume_of_sphere.py
+++ volume_of_sphere.py
@@ -4,9 +4,9 @@ def calculate_volume_of_sphere(radius):
 	return area
 
 radius1 = 30
-area1 = compute_area_of_circle(radius1)
+area1 = calculate_volume_of_sphere(radius1)
 print(f"The area of circle with radius {radius1} is: {area1}")
 
 radius2 = 40
-area2 = compute_area_of_circle(radius2)
+area2 = calculate_volume_of_sphere(radius2)
 print(f"The area of circle with radius {radius2} is: {area2}")
